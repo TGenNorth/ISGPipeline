cf569667480a967f4489c6857057b32e41965aac
Jamie Beckstrom-Sternberg <jbeckstrom@tgen.org>
2013-03-07 14:51:14 -0700

    Updated picard dependencies to v1.86. Automatically detect quality format before running bwa.

--- src/org/nau/bwa/AlnRunner.java
+++ src/org/nau/bwa/AlnRunner.java
@@ -1,12 +1,12 @@
 package org.nau.bwa;
 
-import org.nau.mummer.*;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import net.sf.picard.fastq.FastqReader;
+import net.sf.picard.util.FastqQualityFormat;
+import net.sf.picard.util.QualityEncodingDetector;
 import org.nau.util.ExternalProcess;
-import org.tgen.commons.utils.CommandFactory;
 
 public class AlnRunner implements Runnable {
 
@@ -30,11 +30,29 @@ public class AlnRunner implements Runnable {
         List<String> ret = new ArrayList<String>();
         ret.add(bwa);
         ret.add("aln");
+        if(isIllumina()){
+            ret.add("-I");
+        }
         ret.add("-f");
         ret.add(saiFile.getAbsolutePath());
         ret.add(refPrefix);
         ret.add(readFile.getAbsolutePath());
         return ret.toArray(new String[ret.size()]);
+    }
+    
+    private boolean isIllumina(){
+        final FastqQualityFormat format = QualityEncodingDetector.detect(
+                new FastqReader(readFile));
+        switch(format){
+            case Illumina:
+                return true;
+            case Solexa:
+                throw new IllegalStateException("Solexa quality scores are not supported by bwa");
+            case Standard:
+                return false;
+            default:
+                throw new IllegalStateException("Could not determine quality for fastq");
+        }
     }
 
     public void run() {
