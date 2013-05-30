5db31d5d0e6072f98d7e93abc20531494d5cbcc0
Jamie Beckstrom-Sternberg <jbeckstrom@tgen.org>
2013-05-30 14:35:47 -0700

    Added additional arguments to CallableLociCommandLineFunction and corresponding tests

--- src/gatk/CallableLociCommandLineFunction.java
+++ src/gatk/CallableLociCommandLineFunction.java
@@ -5,6 +5,7 @@
 package gatk;
 
 import java.io.File;
+import org.broadinstitute.sting.commandline.Argument;
 import org.broadinstitute.sting.commandline.Output;
 /**
  *
@@ -17,6 +18,27 @@ public class CallableLociCommandLineFunction extends GATKCommandLineFunction {
     
     @Output(doc = "summary file")
     public File summary;
+    
+    @Argument(doc = "If the fraction of reads at a base with low mapping quality exceeds this value, the site may be poorly mapped", required=false)
+    public Double frlmq;
+    
+    @Argument(doc = "Maximum read depth before a locus is considered poorly mapped", required=false)
+    public Integer maxDepth;
+    
+    @Argument(doc = "Maximum value for MAPQ to be considered a problematic mapped read.", required=false)
+    public Byte maxLowMAPQ;
+    
+    @Argument(doc = "Minimum quality of bases to count towards depth.", required=false)
+    public Byte minBaseQuality;
+    
+    @Argument(doc = "Minimum mapping quality of reads to count towards depth.", required=false)
+    public Byte minMappingQuality;
+    
+    @Argument(doc = "Minimum QC+ read depth before a locus is considered callable", required=false)
+    public Integer minDepth;
+    
+    @Argument(doc = "Minimum read depth before a locus is considered a potential candidate for poorly mapped", required=false)
+    public Integer minDepthForLowMAPQ;
 
     @Override
     public String analysisName() {
@@ -27,8 +49,14 @@ public class CallableLociCommandLineFunction extends GATKCommandLineFunction {
     public String commandLine() {
         return super.commandLine() 
                 + required("-o", out, "", true, true, "%s")
-                + required("-summary", summary, "", true, true, "%s");
-                
+                + required("-summary", summary, "", true, true, "%s")
+                + optional("-frlmq", frlmq, "", true, true, "%s")
+                + optional("--maxDepth", maxDepth, "", true, true, "%s")
+                + optional("--maxLowMAPQ", maxLowMAPQ, "", true, true, "%s")
+                + optional("--minBaseQuality", minBaseQuality, "", true, true, "%s")
+                + optional("--minMappingQuality", minMappingQuality, "", true, true, "%s")
+                + optional("--minDepth", minDepth, "", true, true, "%s")
+                + optional("--minDepthForLowMAPQ", minDepthForLowMAPQ, "", true, true, "%s");  
     }
     
 }
--- test/gatk/CallableLociCommandLineFunctionTest.java
+++ test/gatk/CallableLociCommandLineFunctionTest.java
@@ -37,19 +37,102 @@ public class CallableLociCommandLineFunctionTest {
     @Test
     public void testCommandLine() {
         System.out.println("commandLine");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        String expResult = createStandardResult();
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineFrlmq() {
+        System.out.println("commandLineFrlmq");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.frlmq = 0.1;
+        String expResult = createStandardResult();
+        expResult += " '-frlmq' '0.1'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMaxDepth() {
+        System.out.println("commandLineMaxDepth");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.maxDepth = -1;
+        String expResult = createStandardResult();
+        expResult += " '--maxDepth' '-1'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMaxLowMAPQ() {
+        System.out.println("commandLineMaxLowMAPQ");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.maxLowMAPQ = 1;
+        String expResult = createStandardResult();
+        expResult += " '--maxLowMAPQ' '1'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMinBaseQuality() {
+        System.out.println("commandLineMinBaseQuality");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.minBaseQuality = 20;
+        String expResult = createStandardResult();
+        expResult += " '--minBaseQuality' '20'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMinMappingQuality() {
+        System.out.println("commandLineMinMappingQuality");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.minMappingQuality = 10;
+        String expResult = createStandardResult();
+        expResult += " '--minMappingQuality' '10'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMinDepth() {
+        System.out.println("commandLineMinDepth");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.minDepth = 4;
+        String expResult = createStandardResult();
+        expResult += " '--minDepth' '4'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineMinDepthForLowMAPQ() {
+        System.out.println("commandLineMinDepthForLowMAPQ");
+        CallableLociCommandLineFunction instance = createStandardInstance();
+        instance.minDepthForLowMAPQ = 10;
+        String expResult = createStandardResult();
+        expResult += " '--minDepthForLowMAPQ' '10'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    private CallableLociCommandLineFunction createStandardInstance(){
         CallableLociCommandLineFunction instance = new CallableLociCommandLineFunction();
         instance.jarFile = new File("GATK.jar");
         instance.inputFile = new File("in.bam");
         instance.referenceFile = new File("ref.fasta");
         instance.out = new File("out.bed");
         instance.summary = new File("out.summary");
-        String expResult = "'java'    '-jar' 'GATK.jar'  '-T'  'CallableLoci'"
+        return instance;
+    }
+    
+    private String createStandardResult(){
+        return "'java'    '-jar' 'GATK.jar'  '-T'  'CallableLoci'"
                 + "  '-R'  'ref.fasta'  '-I'  'in.bam'  '-o'  'out.bed'"
                 + "  '-summary'  'out.summary'";
-        String result = instance.commandLine();
-//        for(int i=0; i<Math.min(result.length(), expResult.length()); i++){
-//            System.out.printf("%c = %c\n", result.charAt(i), expResult.charAt(i));
-//        }
-        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
     }
 }
