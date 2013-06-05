5dd169f5ebe093a449355d2f6ddd71fb5e86c02a
Jamie Beckstrom-Sternberg <jbeckstrom@tgen.org>
2013-06-04 17:39:17 -0700

    Added arguments to DeltaFilter

--- src/mummer/DeltaFilterCommandLineFunction.java
+++ src/mummer/DeltaFilterCommandLineFunction.java
@@ -15,23 +15,48 @@ import org.broadinstitute.sting.commandline.Output;
  */
 public class DeltaFilterCommandLineFunction extends MummerCommandLineFunction {
 
+    @Input(doc = "delta input file")
+    public File inDelta;
+    
+    @Output(doc = "filtered delta output file")
+    public File outDelta;
+    
+    @Argument
+    public Boolean g = false;
+    
+    @Argument(required=false)
+    public Byte i;
+    
+    @Argument(required=false)
+    public Integer l;
+    
     @Argument(doc = "Query alignment using length*identity weighted LIS.")
     public Boolean q = false;
     
     @Argument(doc = "Reference alignment using length*identity weighted LIS.")
     public Boolean r = false;
     
-    @Input(doc = "delta input file")
-    public File inDelta;
+    @Argument(required=false)
+    public Byte u;
     
-    @Output(doc = "filtered delta output file")
-    public File outDelta;
-
+    @Argument(required=false)
+    public Byte o;
+    
+    @Override
+    public String analysisName() {
+        return "delta-filter";
+    }
+    
     @Override
     public String commandLine() {
         return required(new File(mummerDir, "delta-filter"))
-                + conditional(r, "-r", true, "%s")
+                + conditional(g, "-g", true, "%s")
+                + optional("-i", i, "", true, true, "%s")
+                + optional("-l", l, "", true, true, "%s")
                 + conditional(q, "-q", true, "%s")
+                + conditional(r, "-r", true, "%s")
+                + optional("-u", u, "", true, true, "%s")
+                + optional("-o", o, "", true, true, "%s")
                 + required(inDelta)
                 + required(">", false)
                 + required(outDelta);
--- test/mummer/DeltaFilterCommandLineFunctionTest.java
+++ test/mummer/DeltaFilterCommandLineFunctionTest.java
@@ -25,11 +25,48 @@ public class DeltaFilterCommandLineFunctionTest {
     @Test
     public void testCommandLine() {
         System.out.println("commandLine");
-        DeltaFilterCommandLineFunction instance = new DeltaFilterCommandLineFunction();
-        instance.mummerDir = new File("mummer");
-        instance.inDelta = new File("in.delta");
-        instance.outDelta = new File("out.delta");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
         String expResult = "'mummer/delta-filter' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_g() {
+        System.out.println("testCommandLineWith_g");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.g = true;
+        String expResult = "'mummer/delta-filter' '-g' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_i() {
+        System.out.println("testCommandLineWith_i");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.i = 0;
+        String expResult = "'mummer/delta-filter' '-i' '0' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_l() {
+        System.out.println("testCommandLineWith_l");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.l = 0;
+        String expResult = "'mummer/delta-filter' '-l' '0' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_q() {
+        System.out.println("testCommandLineWith_q");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.q = true;
+        String expResult = "'mummer/delta-filter' '-q' 'in.delta' > 'out.delta'";
         String result = instance.commandLine();
         assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
     }
@@ -38,16 +75,40 @@ public class DeltaFilterCommandLineFunctionTest {
      * Test of commandLine method, of class DeltaFilterCommandLineFunction.
      */
     @Test
-    public void testCommandLineWithOptions() {
-        System.out.println("commandLineWithOptions");
+    public void testCommandLineWith_r() {
+        System.out.println("commandLineWith_r");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.r = true;
+        String expResult = "'mummer/delta-filter' '-r' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_u() {
+        System.out.println("testCommandLineWith_u");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.u = 0;
+        String expResult = "'mummer/delta-filter' '-u' '0' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    @Test
+    public void testCommandLineWith_o() {
+        System.out.println("testCommandLineWith_o");
+        DeltaFilterCommandLineFunction instance = createRequiredDeltaFilter();
+        instance.o = 75;
+        String expResult = "'mummer/delta-filter' '-o' '75' 'in.delta' > 'out.delta'";
+        String result = instance.commandLine();
+        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+    }
+    
+    private DeltaFilterCommandLineFunction createRequiredDeltaFilter(){
         DeltaFilterCommandLineFunction instance = new DeltaFilterCommandLineFunction();
         instance.mummerDir = new File("mummer");
         instance.inDelta = new File("in.delta");
         instance.outDelta = new File("out.delta");
-        instance.r = true;
-        instance.q = true;
-        String expResult = "'mummer/delta-filter' '-r' '-q' 'in.delta' > 'out.delta'";
-        String result = instance.commandLine();
-        assertArrayEquals(expResult.trim().split("\\s+"), result.trim().split("\\s+"));
+        return instance;
     }
 }
